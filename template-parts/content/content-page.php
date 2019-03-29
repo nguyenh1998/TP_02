0e0905fc1f38cc182db63c718d8d0e76a51ac99b
Nguyen GitHub <>
2019-03-28 23:07:36 -0400

    Modification du header

---
+++ template-parts/content/content-page.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Nineteen
+ * @since 1.0.0
+ */
+
+?>
+//////////////////  accueil-page.php //////////////
+
